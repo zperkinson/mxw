db7165af1072086dcf7900f252daf4293f1a45ce
Zach <z61@zeesixtyone.com>
2025-07-15 10:00:26 -0400

    Add Model O Pro

--- src/glorious.rs
+++ src/glorious.rs
@@ -8,9 +8,11 @@ pub enum Device {
     ModelO = 0x2011,
     ModelD = 0x2012,
     ModelOMinus = 0x2013,
+    WiredModelOPro = 0x2015,
     ModelDMinus = 0x2025,
     WiredModelO = 0x2022,
     WiredModelD = 0x2023,
     WiredModelOMinus = 0x2024,
+    ModelOProWireless = 0x2027,
     ModelD2Pro = 0x2034,
 }
